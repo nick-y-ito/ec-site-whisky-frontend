e0986e875f903753a7bc94d67403156b17afad21
Yusuke Ito <u.skayyyyy@gmail.com>
2023-12-01 03:37:45 -0800

    feat: make the title responsive

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -4,8 +4,8 @@ import { CartIcon } from '@/components/CartIcon';
 export const Header = () => {
   return (
     <header className="w-full h-14 px-5 bg-stone-800 flex items-center">
-      <Link href="/" className='h-full mr-auto flex items-center'>
-        <h1 className="text-lg text-gray-200">Whisky Voyage</h1>
+      <Link href="/" className="h-full mr-auto flex items-center">
+        <div className="text-lg text-gray-200">Whisky Voyage</div>
       </Link>
       <CartIcon />
     </header>
