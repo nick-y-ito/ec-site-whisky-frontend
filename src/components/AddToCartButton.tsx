fad11f0de69ff1e1e52fe83d32873cb29bd05b6d
Yusuke Ito <u.skayyyyy@gmail.com>
2023-12-01 01:01:16 -0800

    feat: add animation when item added to cart

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,19 +1,41 @@
 import { useAppDispatch } from '@/lib/redux/hooks';
 import { increment } from '@/lib/redux/slices/cartSlice';
+import { cn } from '@/lib/utils';
+import { useEffect, useState } from 'react';
+import { FaCheck } from 'react-icons/fa';
 
 interface CartIconProps {
   itemId: number;
 }
 
 export const AddToCartButton = ({ itemId }: CartIconProps) => {
+  const [added, setAdded] = useState(false);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAdded(false);
+    }, 3000);
+  }, [added]);
 
   return (
     <button
-      className="h-10 p-4 flex justify-center items-center border-2 bg-black hover:opacity-60 text-white rounded-xl"
-      onClick={() => dispatch(increment({ itemId: itemId }))}
+      className={cn(
+        'h-10 p-4 flex justify-center items-center rounded-xl bg-black text-white',
+        added ? 'border border-black bg-white text-black' : 'hover:opacity-60',
+      )}
+      onClick={() => {
+        dispatch(increment({ itemId: itemId }));
+        setAdded(true);
+      }}
     >
-      Add to Cart
+      {added ? (
+        <div className="flex items-center gap-2">
+          Added <FaCheck />
+        </div>
+      ) : (
+        'Add to Cart'
+      )}
     </button>
   );
 };
