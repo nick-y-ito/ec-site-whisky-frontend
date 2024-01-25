251c70b4da30f38f011f0b90144916ccd0db88f1
Yusuke Ito <u.skayyyyy@gmail.com>
2024-01-24 16:35:24 -0800

    feat: apply debounce to keyword filter

--- src/components/filter/KeywordFilter.tsx
+++ src/components/filter/KeywordFilter.tsx
@@ -1,33 +1,31 @@
 'use client';
 
 import { useQueryParams } from '@/lib/hooks/useQueryParams';
-import { useState } from 'react';
 import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
+import { useDebouncedCallback } from 'use-debounce';
 
 export const KeywordFilter = () => {
   const { params, replaceParams } = useQueryParams();
-  const [inputKeyword, setInputKeyword] = useState('');
   const paramKeyword = params.get('keyword') || '';
 
   /**
    * Update the URL query string with the keyword filter
    */
-  const handleChange = (query: string) => {
-    setInputKeyword(query);
+  const handleChange = useDebouncedCallback((query: string) => {
     if (query) {
       params.set('keyword', query);
     } else {
       params.delete('keyword');
     }
     replaceParams();
-  };
+  }, 300);
 
   return (
     <div className="h-[35px] border border-black flex items-center relative">
       <input
         className="h-full w-full"
         type="text"
-        value={inputKeyword || paramKeyword}
+        defaultValue={paramKeyword}
         onChange={(e) => handleChange(e.target.value)}
       />
       <HiOutlineMagnifyingGlass className="absolute right-2" />
